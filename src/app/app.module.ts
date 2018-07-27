38afc7c9115e1f355cdd94cfd7d7b8d3912ee0fc
sala02 <sala02@targettrust.local>
2018-07-26 21:35:06 -0300

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ContatoComponent } from './contato/contato.component';
+import { RouterModule } from '@angular/router';
+import { ROTAS } from './routes';
+import { ProdutosComponent } from './produtos/produtos.component';
+import { ProdutoDetalheComponent } from './produtos/produto-detalhe/produto-detalhe.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabecalhoComponent,
+    RodapeComponent,
+    InicioComponent,
+    ContatoComponent,
+    ProdutosComponent,
+    ProdutoDetalheComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROTAS),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
